d20684413fb1fc953f021a265632752b843dc57a
Prabhat Ranjan <prabhat@vinsol.com>
2022-09-13 10:53:58 +0530

    setup UI for buttons

--- src/Component/ActionContainer.js
+++ src/Component/ActionContainer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function ActionContainer() {
   return (
-    <div className='ActionsContainer'>ActionContainer</div>
+    <div className='ActionsContainer'>
+      <button className='Button Attack'>Action</button>
+      <button className='Button SpecialAttack'>Special Attack</button>
+      <button className='Button Heal'>Heal</button>
+      <button className='Button GiveUp'>Give Up</button>
+    </div>
   )
 }
 
