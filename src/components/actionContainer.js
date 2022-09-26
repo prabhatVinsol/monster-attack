454c9c90dc184d747de0cfb9a58418c5849ed84e
Prabhat Ranjan <prabhat@vinsol.com>
2022-09-26 14:15:10 +0530

    Remove arrow function

--- src/components/actionContainer.js
+++ src/components/actionContainer.js
@@ -5,16 +5,16 @@ function ActionContainer({ monsterTurn, attack, specialAttack, heal, giveup, sta
   if (started) {
     return (
       <div className='actions-container'>
-        <button type="button" className={monsterTurn ? 'button disbaled' :'button attack'} onClick={() => attack()}>Action</button>
-        <button type="button" className={disableSpecialAttack || monsterTurn ? 'button disbaled' : 'button special-attack'} disabled={disableSpecialAttack} onClick={() => specialAttack()}>Special Attack</button>
-        <button type="button" className={monsterTurn ? 'button disbaled' :'button heal'} onClick={() => heal()}>Heal</button>
-        <button type="button" className='button giveUp' onClick={() => giveup()}>Give Up</button>
+        <button type="button" className={monsterTurn ? 'button disbaled' :'button attack'} onClick={attack}>Action</button>
+        <button type="button" className={disableSpecialAttack || monsterTurn ? 'button disbaled' : 'button special-attack'} disabled={disableSpecialAttack} onClick={specialAttack}>Special Attack</button>
+        <button type="button" className={monsterTurn ? 'button disbaled' :'button heal'} onClick={heal}>Heal</button>
+        <button type="button" className='button giveUp' onClick={giveup}>Give Up</button>
       </div>
     )
   } else {
     return (
       <div className='actions-container'>
-        <button className='button attack' onClick={() => start()}>Start</button>
+        <button className='button attack' onClick={start}>Start</button>
       </div>
     )
   }
